67804f42a2476e6c38c52c20a8e71d49edd3f7ea
sabrsabrsabrsabr <rahmatullayivdoston@gmail.com>
2022-06-06 15:26:01 +0300

    js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// var firstName = prompt("Ismingizni kiriting?");
+// var age = prompt("Yoshingizni kiriting?");
+// var isMarried = prompt("Uylanganmisiz");
+// var interest = prompt("qaysi sportga qiziqasiz")
+// var answerTheQuestions = prompt("<label> elementi qaysi elementi uchun yorliq yaratish uchun ishlatiladi");
+// var answerTheQuestions2 = prompt("<qaysi> tegi atamalarni ko'rsatish yoki qisqartmalarni kengaytirish uchun ishlatiladi.");
+// var answerTheQuestions3 = prompt("<qaysi> tegi iqtibos manbalari, san'at asarlari yoki san'at ob'ektlarini ko'rsatish uchun ishlatiladi.");
+// var dataTypes = prompt("<Data Types> lardi yozinig");
+// var howCase = prompt("<FirstName> qanaqa /case/ da yozilgan")
+// var howcases  = prompt("Bizar qanaqa /case/lardan foydalanamiz")
+
+// console.log(
+//   firstName + '\n' +
+//   age + '\n' +
+//   isMarried + '\n' +
+//   interest + '\n' +
+//   answerTheQuestions + '\n' +
+//   answerTheQuestions2 + '\n' +
+//   answerTheQuestions3 + '\n' +
+//   dataTypes + '\n' +
+//   howCase + '\n' +
+//   howcases);
